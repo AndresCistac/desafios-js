7020670b15ff11dc8a0d8b3a344c3462f9ab72ff
Andres Cistac <andrescistac.iacc@gmail.com>
2021-05-16 13:17:31 -0300

    desafio 3

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 //desafio 2
 
-var usuario = prompt("Ingresa tu nombre de usuario:");
+// var usuario = prompt("Ingresa tu nombre de usuario:");
 
-if (usuario == "Carlitos123" || usuario == "carlitos123") {
-    let password = prompt("Ingresa tu password");
+// if (usuario == "Carlitos123" || usuario == "carlitos123") {
+//     let password = prompt("Ingresa tu password");
 
-    if (password === "123456") {
-        alert("Bienvenido " + usuario);
-    } else {
-        alert("Contraseña erronea");
+//     if (password === "123456") {
+//         alert("Bienvenido " + usuario);
+//     } else {
+//         alert("Contraseña erronea");
+//     }
+// } else {
+//     alert("Usuario incorrecto");
+// }
+
+//desafio 3
+for (let i = 1; i <= 30 ; i++){
+    for (let x = 9; x <= 13 ; x++){
+    var ingresarNombre = prompt("Ingresar nombre y apellido");
+    alert("Hola " + ingresarNombre + " su turno es a las " +x+ " hs del dia " +i+ " de Junio");
     }
-} else {
-    alert("Usuario incorrecto");
 }
+
+/* seria un sistema de turnos que entrega turnos entre las 9 y las 13 hs, 
+al cumplirse esos turnos empieza a solicitar turnos para el dia siguiente  */
