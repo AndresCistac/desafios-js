39d0c56253a8686c32e3d02e6460f389f2744ccd
Andres Cistac <andrescistac.iacc@gmail.com>
2021-05-20 18:26:48 -0300

    desafio 4 complementario

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-//desafio 4
+//desafio 4 complementario
 
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
 const descuento = i => i * 0.15;
+const interes =  x => x * 0.10;
 
 let precioConsulta = 1200;
 let protocolo = 500;
 
 let total = suma(resta(precioConsulta, descuento(precioConsulta)), protocolo);
-console.log("El precio de la consulta con el 15% de dto de su obra social mas el protocolo COVID seria $" + total)
+console.log("El precio de la consulta con el 15% de dto de su obra social mas el protocolo COVID seria $" + total);
 
+let pago = prompt("Por favor ingrese si pagaria con Debito/Credito");
+
+
+if((pago == "Debito") || (pago == "debito")){
+ alert("El total a abonar seria $" + total);
+}else{
+ alert("El total a abonar con un 10% de recargo seria $" + suma(total, interes(total)));
+}
 
 
