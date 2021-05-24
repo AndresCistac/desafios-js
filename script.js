07f78d482806d70d2cd9488ecc6b6cb9211c32e5
Andres Cistac <andrescistac.iacc@gmail.com>
2021-05-24 19:56:27 -0300

    desafio 6

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 // Desafio 5
 
+// class Turno{ 
+//     constructor (nombre, apellido, afiliado, especialidad){
+//     this.nombre = nombre;
+//     this.apellido = apellido;
+//     this.afiliado = afiliado;
+//     this.especialidad = especialidad;
+//     }
+//     solicitud() {
+//         console.log("Se solicita turno para "+ this.nombre + " " + this.apellido);
+//     }
+   
+//     aceptar() { 
+//         var okey =  prompt("Si es correcta la informacion ingresar OK");
+
+//         if(okey == "OK") {
+//             console.log("Se confirma turno para el Sr/Sra " + this.apellido + " especialidad: " + this.especialidad + ".");
+//         }
+//     }
+    
+// }
+
+// const turno1 = new Turno ("Alicia", "Almada", 212376, "Dermatologia");
+// const turno2 = new Turno ("Leandro", "Perez", 207426,"Clinico");
+
+// turno1.solicitud();
+// turno1.aceptar();
+
+        //desafio 6 y primer entrega final
+
 class Turno{ 
     constructor (nombre, apellido, afiliado, especialidad){
     this.nombre = nombre;
@@ -14,15 +43,30 @@ class Turno{
     aceptar() { 
         var okey =  prompt("Si es correcta la informacion ingresar OK");
 
-        if(okey == "OK") {
+        if((okey == "OK") || (okey == "ok")) {
             console.log("Se confirma turno para el Sr/Sra " + this.apellido + " especialidad: " + this.especialidad + ".");
+        } else{
+            console.log("Le pedimos por favor volver a ingresar los datos, Muchas gracias.")
         }
+    
     }
     
 }
+const turnos = [];
 
-const turno1 = new Turno ("Alicia", "Almada", 212376, "Dermatologia");
-const turno2 = new Turno ("Leandro", "Perez", 207426,"Clinico");
 
-turno1.solicitud();
-turno1.aceptar();
+turnos.push (new Turno ("Alicia", "Almada", 212376, "Ginecologia"));
+turnos.push (new Turno ("Leandro", "Perez", 207426,"Clinica"));
+turnos.push (new Turno ("Alejandra", "Marques", 605446,"Ginecologia"));
+turnos.push (new Turno ("Marta", "Aguirre", 862356, "Guardia"));
+console.log(turnos.length);
+
+for (const turno of turnos){
+    console.log(turno.solicitud());
+}
+const ginecologia = turnos.filter(turno => turno.especialidad =="Ginecologia");
+console.log(ginecologia)
+
+
+// desafio 6 complementario
+
